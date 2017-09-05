e5f6dca672bbe61fa8f5a9d33d04dfca07539237
mindy-rainbow <mindy.rainbow@gmail.com>
2017-09-05 08:58:03 +0800

    fix application

--- app/src/main/java/com/zxcioc/creditcheck/MyApplication.java
+++ app/src/main/java/com/zxcioc/creditcheck/MyApplication.java
@@ -12,13 +12,10 @@ public class MyApplication extends Application {
 
     public static MyApplication application = null;
 
-    public MyApplication(){
-        application =  this;
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
+        application =  this;
         //初始化工具类
         //https://github.com/Blankj/AndroidUtilCode
         Utils.init(this);
